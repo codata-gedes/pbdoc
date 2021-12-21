3caae8f7ab5dd02d5bcde5dfc5d6677b117affc9
Wendell Clive Santos de Lira <wendell@codata.pb.gov.br>
2021-12-21 09:54:04 -0300

    Correção de não carrega paginação de usuario

--- siga/src/main/java/br/gov/jfrj/siga/vraptor/DpPessoaController.java
+++ siga/src/main/java/br/gov/jfrj/siga/vraptor/DpPessoaController.java
@@ -166,7 +166,7 @@ public class DpPessoaController extends SigaSelecionavelControllerSupport<DpPess
 		String buscarFechadas = param("buscarFechadas");
 		flt.setBuscarFechadas(buscarFechadas != null ? Boolean.valueOf(buscarFechadas) : false);
 		String buscarSubstitutos = param("buscarSubstitutos");
-		if (buscarSubstitutos != null) {
+		if (buscarSubstitutos.equals(true)) {
 			flt.setBuscarSubstitutos(true);
 			flt.setId(getTitular().getId());
 		} else {
