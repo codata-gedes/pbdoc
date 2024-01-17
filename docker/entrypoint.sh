a1f2a75fcd99d477a6bc0d7af0e6a24f6c8e049c
Livio Ribeiro <livio@codata.pb.gov.br>
2024-01-17 12:20:58 -0300

    melhora processamento de variáveis de ambiente e .dockerignore

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,69 +4,70 @@ set -e
 
 standalone_xml=$JBOSS_HOME/standalone/configuration/standalone.xml
 
-if [ -z "$DATABASE_URL" ]; then
-    echo 'Faltando $DATABASE_URL'
-    exit 1
-fi
+configure() {
+    if [ -n "$DATABASE_URL" ]; then
+        if [ -z "$DATABASE_USERNAME" ]; then
+            echo 'Faltando $DATABASE_USERNAME'
+            exit 1
+        fi
+        
+        if [ -z "$DATABASE_PASSWORD" ]; then
+            echo 'Faltando $DATABASE_PASSWORD'
+            exit 1
+        fi
 
-if [ -z "$DATABASE_USERNAME" ]; then
-    echo 'Faltando $DATABASE_USERNAME'
-    exit 1
-fi
+        sed --in-place --regexp-extended \
+            --expression "s|<connection-url>.*?</connection-url>|<connection-url>$DATABASE_URL</connection-url>|" \
+            --expression "s|<user-name>.*?</user-name>|<user-name>$DATABASE_USERNAME</user-name>|" \
+            --expression "s|<password>.*?</password>|<password>$DATABASE_PASSWORD</password>|" \
+            $standalone_xml
+    fi
 
-if [ -z "$DATABASE_PASSWORD" ]; then
-    echo 'Faltando $DATABASE_PASSWORD'
-    exit 1
-fi
+    if [ -n "$DATABASE_MIN_POOL_SIZE" ]; then
+        sed --in-place --regexp-extended "s|<min-pool-size>.*?</min-pool-size>|<min-pool-size>$DATABASE_MIN_POOL_SIZE</min-pool-size>|" $standalone_xml
+    fi
 
-sed -i -E \
-    -e "s|<connection-url>.*?</connection-url>|<connection-url>$DATABASE_URL</connection-url>|" \
-    -e "s|<user-name>.*?</user-name>|<user-name>$DATABASE_USERNAME</user-name>|" \
-    -e "s|<password>.*?</password>|<password>$DATABASE_PASSWORD</password>|" \
-    $standalone_xml
+    if [ -n "$DATABASE_MAX_POOL_SIZE" ]; then
+        sed --in-place --regexp-extended "s|<max-pool-size>.*?</max-pool-size>|<max-pool-size>$DATABASE_MAX_POOL_SIZE</max-pool-size>|" $standalone_xml
+    fi
 
-if [ -n "$DATABASE_MIN_POOL_SIZE" ]; then
-    sed -i -E "s|<min-pool-size>.*?</min-pool-size>|<min-pool-size>$DATABASE_MIN_POOL_SIZE</min-pool-size>|" $standalone_xml
-fi
+    if [ -n "$PBDOC_CABECALHO_TITULO" ]; then
+        sed --in-place --regexp-extended "s|<property name=\"sigaex.modelos.cabecalho.titulo\" value=\".*?\"/>|<property name=\"sigaex.modelos.cabecalho.titulo\" value=\"$PBDOC_CABECALHO_TITULO\"/>|" $standalone_xml
+    fi
 
-if [ -n "$DATABASE_MAX_POOL_SIZE" ]; then
-    sed -i -E "s|<max-pool-size>.*?</max-pool-size>|<max-pool-size>$DATABASE_MAX_POOL_SIZE</max-pool-size>|" $standalone_xml
-fi
+    if [ -n "$PBDOC_CABECALHO_SUBTITULO" ]; then
+        sed --in-place --regexp-extended "s|<property name=\"sigaex.modelos.cabecalho.subtitulo\" value=\".*?\"/>|<property name=\"sigaex.modelos.cabecalho.titulo\" value=\"$PBDOC_CABECALHO_SUBTITULO\"/>|" $standalone_xml
+    fi
 
-if [ -n "$PBDOC_CABECALHO_TITULO" ]; then
-    sed -i -E "s|<property name=\"sigaex.modelos.cabecalho.titulo\" value=\".*?\"/>|<property name=\"sigaex.modelos.cabecalho.titulo\" value=\"$PBDOC_CABECALHO_TITULO\"/>|" $standalone_xml
-fi
+    if [ -n "$PBDOC_PREFEITURA" ]; then
+        sed --in-place --regexp-extended 's|<property name="siga.prefeitura" value=".*?"/>|<property name="siga.prefeitura" value="true"/>|' $standalone_xml
+    fi
 
-if [ -n "$PBDOC_CABECALHO_SUBTITULO" ]; then
-    sed -i -E "s|<property name=\"sigaex.modelos.cabecalho.subtitulo\" value=\".*?\"/>|<property name=\"sigaex.modelos.cabecalho.titulo\" value=\"$PBDOC_CABECALHO_SUBTITULO\"/>|" $standalone_xml
-fi
+    if [ -n "$PBDOC_PREFEITURA_CABECALHO" ]; then
+        sed --in-place --regexp-extended "s|<property name=\"siga.prefeitura.cabecalho\" value=\".*?\"/>|<property name=\"siga.prefeitura.cabecalho\" value=\"$PBDOC_PREFEITURA_CABECALHO\"/>|" $standalone_xml
+    fi
 
-if [ -n "$PBDOC_PREFEITURA" ]; then
-    sed -i -E "s|<property name=\"siga.prefeitura\" value=\".*?\"/>|<property name=\"siga.prefeitura\" value=\"$PBDOC_PREFEITURA\"/>|" $standalone_xml
-fi
+    if [ -n "$PBDOC_PREFEITURA_TEMA_COR" ]; then
+        sed --in-place --regexp-extended "s|<property name=\"siga.prefeitura.tema.cor\" value=\".*?\"/>|<property name=\"siga.prefeitura.tema.cor\" value=\"$PBDOC_PREFEITURA_TEMA_COR\"/>|" $standalone_xml
+    fi
 
-if [ -n "$PBDOC_PREFEITURA_CABECALHO" ]; then
-    sed -i -E "s|<property name=\"siga.prefeitura.cabecalho\" value=\".*?\"/>|<property name=\"siga.prefeitura.cabecalho\" value=\"$PBDOC_PREFEITURA_CABECALHO\"/>|" $standalone_xml
-fi
+    if [ -n "$PBDOC_RELATORIO_TITULO" ]; then
+        sed --in-place --regexp-extended "s|<property name=\"siga.relat.titulo\" value=\".*?\"/>|<property name=\"siga.relat.titulo\" value=\"$PBDOC_RELATORIO_TITULO\"/>|" $standalone_xml
+    fi
 
-if [ -n "$PBDOC_PREFEITURA_TEMA_COR" ]; then
-    sed -i -E "s|<property name=\"siga.prefeitura.tema.cor\" value=\".*?\"/>|<property name=\"siga.prefeitura.tema.cor\" value=\"$PBDOC_PREFEITURA_TEMA_COR\"/>|" $standalone_xml
-fi
+    if [ -n "$PBDOC_RELATORIO_SUBTITULO" ]; then
+        sed --in-place --regexp-extended "s|<property name=\"siga.relat.subtitulo\" value=\".*?\"/>|<property name=\"siga.relat.subtitulo\" value=\"$PBDOC_RELATORIO_SUBTITULO\"/>|" $standalone_xml
+    fi
 
-if [ -n "$PBDOC_RELATORIO_TITULO" ]; then
-    sed -i -E "s|<property name=\"siga.relat.titulo\" value=\".*?\"/>|<property name=\"siga.relat.titulo\" value=\"$PBDOC_RELATORIO_TITULO\"/>|" $standalone_xml
-fi
+    if [ -n "$PBDOC_CARIMBO_TEXTO_SUPERIOR" ]; then
+        sed --in-place --regexp-extended "s|<property name=\"sigaex.carimbo.texto.superior\" value=\".*?\"/>|<property name=\"sigaex.carimbo.texto.superior\" value=\"$PBDOC_CARIMBO_TEXTO_SUPERIOR\"/>|" $standalone_xml
+    fi
 
-if [ -n "$PBDOC_RELATORIO_SUBTITULO" ]; then
-    sed -i -E "s|<property name=\"siga.relat.subtitulo\" value=\".*?\"/>|<property name=\"siga.relat.subtitulo\" value=\"$PBDOC_RELATORIO_SUBTITULO\"/>|" $standalone_xml
-fi
+    if [ -n "$PBDOC_FLYWAY_MIGRATE" ]; then
+        sed --in-place --regexp-extended 's|<property name="siga.flyway.migrate" value=".*?"/>|<property name="siga.flyway.migrate" value="true"/>|' $standalone_xml
+    fi
+}
 
-if [ -n "$PBDOC_CARIMBO_TEXTO_SUPERIOR" ]; then
-    sed -i -E "s|<property name=\"sigaex.carimbo.texto.superior\" value=\".*?\"/>|<property name=\"sigaex.carimbo.texto.superior\" value=\"$PBDOC_CARIMBO_TEXTO_SUPERIOR\"/>|" $standalone_xml
-fi
-
-if [ -n "$PBDOC_FLYWAY_MIGRATE" ]; then
-    sed -i -E "s|<property name=\"siga.flyway.migrate\" value=\".*?\"/>|<property name=\"siga.flyway.migrate\" value=\"$PBDOC_FLYWAY_MIGRATE\"/>|" $standalone_xml
-fi
+configure
 
 exec $JBOSS_HOME/bin/standalone.sh -b 0.0.0.0
