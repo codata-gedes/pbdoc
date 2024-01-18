69e73ffb098410a8adb7ef7fd30b4f424b9441e6
Livio Ribeiro <livio@codata.pb.gov.br>
2024-01-18 10:47:49 -0300

    adiciona parâmetros faltantes no standalone.xml

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,6 +13,13 @@ is_true() {
     return 1 # false
 }
 
+sed_replace() {
+    property=$1
+    replacement=$2
+    shift 2
+    sed --in-place --regexp-extended "s|<property name=\"($property)\" value=\".*?\"\s*/>|<property name=\"\\1\" value=\"$replacement\"/>|" $standalone_xml
+}
+
 configure() {
     if [ -n "$DATABASE_URL" ]; then
         if [ -z "$DATABASE_USERNAME" ]; then
@@ -40,40 +47,64 @@ configure() {
         sed --in-place --regexp-extended "s|<max-pool-size>.*?</max-pool-size>|<max-pool-size>$DATABASE_MAX_POOL_SIZE</max-pool-size>|" $standalone_xml
     fi
 
+    if [ -n "$PBDOC_BASE_URL" ]; then
+        sed_replace 'sigaex.modelos.cabecalho.titulo' "$PBDOC_CABECALHO_TITULO"
+    fi
+
     if [ -n "$PBDOC_CABECALHO_TITULO" ]; then
-        sed --in-place --regexp-extended "s|<property name=\"sigaex.modelos.cabecalho.titulo\" value=\".*?\"/>|<property name=\"sigaex.modelos.cabecalho.titulo\" value=\"$PBDOC_CABECALHO_TITULO\"/>|" $standalone_xml
+        sed_replace 'sigaex.modelos.cabecalho.titulo' "$PBDOC_CABECALHO_TITULO"
     fi
 
     if [ -n "$PBDOC_CABECALHO_SUBTITULO" ]; then
-        sed --in-place --regexp-extended "s|<property name=\"sigaex.modelos.cabecalho.subtitulo\" value=\".*?\"/>|<property name=\"sigaex.modelos.cabecalho.titulo\" value=\"$PBDOC_CABECALHO_SUBTITULO\"/>|" $standalone_xml
+        sed_replace 'sigaex.modelos.cabecalho.subtitulo' "$PBDOC_CABECALHO_SUBTITULO"
     fi
 
     if is_true "$PBDOC_PREFEITURA"; then
-        sed --in-place --regexp-extended 's|<property name="siga.prefeitura" value=".*?"/>|<property name="siga.prefeitura" value="true"/>|' $standalone_xml
+        sed_replace 'siga.prefeitura' "true"
     fi
 
     if [ -n "$PBDOC_PREFEITURA_CABECALHO" ]; then
-        sed --in-place --regexp-extended "s|<property name=\"siga.prefeitura.cabecalho\" value=\".*?\"/>|<property name=\"siga.prefeitura.cabecalho\" value=\"$PBDOC_PREFEITURA_CABECALHO\"/>|" $standalone_xml
+        sed_replace 'siga.prefeitura.cabecalho' "$PBDOC_PREFEITURA_CABECALHO"
     fi
 
     if [ -n "$PBDOC_PREFEITURA_TEMA_COR" ]; then
-        sed --in-place --regexp-extended "s|<property name=\"siga.prefeitura.tema.cor\" value=\".*?\"/>|<property name=\"siga.prefeitura.tema.cor\" value=\"$PBDOC_PREFEITURA_TEMA_COR\"/>|" $standalone_xml
+        sed_replace 'siga.prefeitura.tema.cor' "$PBDOC_PREFEITURA_TEMA_COR"
     fi
 
     if [ -n "$PBDOC_RELATORIO_TITULO" ]; then
-        sed --in-place --regexp-extended "s|<property name=\"siga.relat.titulo\" value=\".*?\"/>|<property name=\"siga.relat.titulo\" value=\"$PBDOC_RELATORIO_TITULO\"/>|" $standalone_xml
+        sed_replace 'siga.relat.titulo' "$PBDOC_RELATORIO_TITULO"
     fi
 
     if [ -n "$PBDOC_RELATORIO_SUBTITULO" ]; then
-        sed --in-place --regexp-extended "s|<property name=\"siga.relat.subtitulo\" value=\".*?\"/>|<property name=\"siga.relat.subtitulo\" value=\"$PBDOC_RELATORIO_SUBTITULO\"/>|" $standalone_xml
+        sed_replace 'siga.relat.subtitulo' "$PBDOC_RELATORIO_SUBTITULO"
     fi
 
     if [ -n "$PBDOC_CARIMBO_TEXTO_SUPERIOR" ]; then
-        sed --in-place --regexp-extended "s|<property name=\"sigaex.carimbo.texto.superior\" value=\".*?\"/>|<property name=\"sigaex.carimbo.texto.superior\" value=\"$PBDOC_CARIMBO_TEXTO_SUPERIOR\"/>|" $standalone_xml
+        sed_replace 'sigaex.carimbo.texto.superior' "$PBDOC_CARIMBO_TEXTO_SUPERIOR"
+    fi
+
+    if [ -n "$PBDOC_JWT_SECRET" ]; then
+        sed_replace 'siga.jwt.secret' "$PBDOC_JWT_SECRET"
+    fi
+
+    if [ -n "$PBDOC_SINC_PASSWORD" ]; then
+        sed_replace 'siga.sinc.password' "$PBDOC_SINC_PASSWORD"
+    fi
+
+    if [ -n "$PBDOC_WEBDAV_SENHA" ]; then
+        sed_replace 'sigaex.webdav.senha' "$PBDOC_WEBDAV_SENHA"
+    fi
+
+    if [ -n "$PBDOC_RECAPTCHA_KEY" ]; then
+        sed_replace 'siga.recaptcha.key' "$PBDOC_RECAPTCHA_KEY"
+    fi
+
+    if [ -n "$PBDOC_RECAPTCHA_PASSWORD" ]; then
+        sed_replace 'siga.recaptcha.pwd' "$PBDOC_RECAPTCHA_PASSWORD"
     fi
 
     if is_true "$PBDOC_FLYWAY_MIGRATE"; then
-        sed --in-place --regexp-extended 's|<property name="siga.flyway.migrate" value=".*?"/>|<property name="siga.flyway.migrate" value="true"/>|' $standalone_xml
+        sed_replace 'siga.flyway.migrate' "true"
     fi
 }
 
